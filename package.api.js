83996a8b8a7850b1e3f69e5b368aecb8c96d31a3
Valex4 <yam778123@gmail.com>
2023-06-27 20:37:24 -0600

    Segundo commit

--- package.api.js
+++ package.api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export  const createPackage = async(pkg)=>{
-    return await axios.post("http://localhost:3000/package/create", pkg);
+    return await axios.post("http://52.22.219.247:3000/package/create", pkg);
 }
